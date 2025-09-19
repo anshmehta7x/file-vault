322ea46d9bcbde927e39d76d8c312624949cd25c
Ansh Mehta <anshkapilmehta@gmail.com>
2025-09-20 00:14:22 +0530

    setup: initial setup + docker compose

---
+++ src/main/java/dev/anshmehta/filevault/FilevaultApplication.java
@@ -0,0 +1,13 @@
+package dev.anshmehta.filevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilevaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilevaultApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/anshmehta/filevault/model/User.java
@@ -0,0 +1,4 @@
+package dev.anshmehta.filevault.model;
+
+public class User {
+}
